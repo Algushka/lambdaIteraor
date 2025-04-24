d2e5c903e319249c269860b865f61985593737d9
ASUS <successolga118@gmail.com>
2025-04-24 19:10:17 +0300

    Lambda iterators

--- src/Main2.java
+++ src/Main2.java
@@ -3,12 +3,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
-       List <BankAccount> bankAccounts = new ArrayList<>(List.of(new BankAccount(new Person("Jack", "Naumov",
-               "g@gmail.com"), "637821371319", 70000 ),
-               new BankAccount(new Person("Nick", "Starostin",
-                       "a@gmail.com"), "36781927332", 1000000 )));
+        List <BankAccount> bankAccounts = new ArrayList<>(List.of(new BankAccount(new Person("Jack", "Naumov",
+                        "g@gmail.com"), "637821371319", 70000 ),
+                new BankAccount(new Person("Nick", "Starostin",
+                        "a@gmail.com"), "36781927332", 1000000 )));
 
         System.out.println("emails:" + transform(bankAccounts, s -> s.getPerson().email));
         System.out.println(transform(bankAccounts, (s) -> s.getPerson().getlName() +" " + s.getPerson().getfName().charAt(0) + ".; IBAN: DE" + s.getIBAN() + "; " + s.getPerson().getEmail()));
@@ -16,16 +16,17 @@ public class Main {
 
     }
     public static <T,R> List <R> transform (List<T> list, Function <T,R> function){
-        //<T> iterator = list.iterator();
+        Iterator <T> iterator = list.iterator();
         //while (iterator.hasNext()) {
-           // T str = iterator.next();
-          List<R> result = new ArrayList<>();
-           for (T element:list)  {
-               result.add(function.apply(element));
-           }
-           return result;
+        // T str = iterator.next();
+        List<R> result = new ArrayList<>();
+        while (iterator.hasNext()) {
+            T element = iterator.next();
+            result.add(function.apply(element));
         }
-
+        return result;
     }
+
+}
 
 
